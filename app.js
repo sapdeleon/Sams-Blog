f09dc9ba1806a6ec008d45a566bf9147f67461d2
tomzie27s <tomzie27s@gmail.com>
2022-01-16 01:06:38 -0500

    Applied MVC approach and completed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbconfig");
 require("dotenv").config();
-const Blog = require("./models/blog");
+const blogRoute = require("./routes/blogRoutes");
 
 const app = express();
 
@@ -22,60 +22,11 @@ app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
 
+// blog routes
+app.use("/blogs", blogRoute);
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
-});
-
-// blog routes
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { blogs: result, title: "All blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
 
 // // 404 page - page does not exist
